b9eeeb1b8361a0a39de4ecfe7b45189d1fb84594
Panashe M. Fundira <fundirap@gmail.com>
2016-04-25 22:05:19 -0400

    smoke test - user can visit homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'workouts/index'
+
+  root to: "workouts#index"
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
